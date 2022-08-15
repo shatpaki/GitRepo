6c24cb8b7f7913cc765c69bbc82ff1b06b7ced57
SachinH <sachin.hatpaki@gmail.com>
2022-08-15 18:56:59 +0530

    Added toString method

--- src/com/java/Employee.java
+++ src/com/java/Employee.java
@@ -17,5 +17,11 @@ public class Employee {
 	public void setAge(int age) {
 		this.age = age;
 	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + "]";
+	}
+	
+	
 
 }
