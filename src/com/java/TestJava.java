ee03293d1c5cc96cd2315a4fa52995a36719f4b3
SachinH <sachin.hatpaki@gmail.com>
2022-08-15 16:14:12 +0530

    New Remote Repository creation

---
+++ src/com/java/TestJava.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test From Github 15th August");
+//		System.out.println("Git Udemy Course beginning");
+		
+	}
+
+}
