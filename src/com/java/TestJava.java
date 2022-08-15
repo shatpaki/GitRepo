b5dff255c0b569838e41e66c324a1efdbf5d305a
SachinH <sachin.hatpaki@gmail.com>
2022-08-15 16:57:53 +0530

    Git Udemy sysout stmt added

--- src/com/java/TestJava.java
+++ src/com/java/TestJava.java
@@ -5,7 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		
 		System.out.println("Test From Github 15th August");
-//		System.out.println("Git Udemy Course beginning");
+		System.out.println("Git Udemy Course beginning");
 		
 	}
 
