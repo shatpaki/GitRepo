3a57b7997dddf92e6de1827c229c07b91f3f2622
SachinH <sachin.hatpaki@gmail.com>
2022-09-11 19:12:18 +0530

    Conflict pull-2 main.java

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 		// Test new Branch - devSach
 		// Test Upstream update 1
 		System.out.println("Test Main :");
+		System.out.println("Conflict during pull in Eclipse");
 	}
 
 }
