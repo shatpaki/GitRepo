e951ebd466c1adc2e976b1b94ffe38a5a6a3598d
SachinH <sachin.hatpaki@gmail.com>
2022-09-21 11:54:15 +0530

    Removed all Getters and Setters from Employee.java

--- src/com/java/Employee.java
+++ src/com/java/Employee.java
@@ -1,33 +1,8 @@
 package com.java;
 
 public class Employee {
-	//change-2 feature to branch feature2Rebase
-	//change-1 feature to branch feature2Rebase
-	//change-1 master
-	// Test upsteam update 2
-	//Getters and Setters
+	
 	private String name;
 	private int age;
-	private double salary;
-	private String address;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", age=" + age + "]";
-	}
-	
-	
 
 }
